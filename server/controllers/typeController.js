b2e41ea35e94865dd57ab307b854f9320238e08a
vsmaliakou <vsmaliakou@yandex.by>
2024-08-05 15:53:36 +0300

    feat: CRUD for types, brands, devices

--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -3,10 +3,15 @@ const ApiError = require('../error/ApiError');
 
 class TypeController {
   async create(req, res) {
-    
+    const { name } = req.body;
+    const type = await Type.create({ name });
+    return res.json(type);
   }
 
-  async getAll(req, res) {}
+  async getAll(req, res) {
+    const types = await Type.findAll();
+    return res.json(types);
+  }
 }
 
 module.exports = new TypeController();
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const path = require('path');
 const sequelize = require('./db');
 const models = require('./models/models');
 const router = require('./routes/index');
@@ -11,6 +13,8 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
 app.use('/api', router);
 app.use(errorHandler);
 
