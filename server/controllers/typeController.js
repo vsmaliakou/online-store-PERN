ce14c9bd567cd26a85d6beefd4269f54579b515d
vsmaliakou <vsmaliakou@yandex.by>
2024-08-05 13:17:18 +0300

    feat: generic error handling

--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -1,5 +1,10 @@
+const { Type } = require('../models/models');
+const ApiError = require('../error/ApiError');
+
 class TypeController {
-  async create(req, res) {}
+  async create(req, res) {
+    
+  }
 
   async getAll(req, res) {}
 }
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const sequelize = require('./db');
 const models = require('./models/models');
 const router = require('./routes/index');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 
 const PORT = process.env.PORT || 4000;
 
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'WORKING!!!' });
