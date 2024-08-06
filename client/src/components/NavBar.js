2dd9005eae346af2040c942f179efde3ef2603de
vsmaliakou <vsmaliakou@yandex.by>
2024-08-06 13:39:10 +0300

    feat: Product and characteristics viewing page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,11 +2,12 @@ import React, { useContext } from "react";
 import { observer } from "mobx-react-lite";
 import { Navbar, Nav, Button, Container } from "react-bootstrap";
 import { Context } from "..";
-import { NavLink } from "react-router-dom";
-import { SHOP_ROUTE } from "../utils/consts";
+import { NavLink, useNavigate } from "react-router-dom";
+import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from "../utils/consts";
 
 const NavBar = observer(() => {
   const { user } = useContext(Context);
+  const navigate = useNavigate();
 
   return (
     <Navbar bg="dark" variant="dark">
@@ -17,12 +18,17 @@ const NavBar = observer(() => {
 
         {user.isAuth ? (
           <Nav className="ms-auto" style={{ color: "white" }}>
-            <Button variant="outline-light">Админ панель</Button>
+            <Button
+              variant="outline-light"
+              onClick={() => navigate(ADMIN_ROUTE)}
+            >
+              Админ панель
+            </Button>
 
             <Button
               className="ms-2"
               variant="outline-light"
-              onClick={() => user.setIsAuth(false)}
+              onClick={() => navigate(LOGIN_ROUTE)}
             >
               Выйти
             </Button>
--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,10 +1,84 @@
 import React from "react";
+import { Button, Card, Col, Container, Image, Row } from "react-bootstrap";
+import bigStar from "../assets/bigStar.png";
 
 const DevicePage = () => {
+  const device = {
+    id: 5,
+    name: "Atlant",
+    price: 100000,
+    rating: 0,
+    img: "18cb79fe-083e-4e6e-bb6f-9344cbb00d44.jpg",
+  };
+  const description = [
+    { id: 1, title: "Оперативная память", description: "5 гб" },
+    { id: 2, title: "Камера", description: "12 мп" },
+    { id: 3, title: "Процессор", description: "Пентиум 3" },
+    { id: 4, title: "Кол-во ядер", description: "2" },
+    { id: 5, title: "Аккумулятор", description: "4000" },
+  ];
+
   return (
-    <div>
-      DevicePage
-    </div>
+    <Container className="mt-3">
+      <Row>
+        <Col md={4}>
+          <Image width={300} height={300} src={device.img} />
+        </Col>
+
+        <Col md={4}>
+          <Row className="d-flex flex-column align-items-center">
+            <h2 className="w-auto">{device.name}</h2>
+
+            <div
+              className="d-flex justify-content-center align-items-center"
+              style={{
+                background: `url(${bigStar}) no-repeat center center`,
+                width: 240,
+                height: 240,
+                backgroundSize: "cover",
+                fontSize: 64,
+              }}
+            >
+              {device.rating}
+            </div>
+          </Row>
+        </Col>
+
+        <Col md={4}>
+          <Card
+            className="d-flex flex-column align-items-center justify-content-around"
+            style={{
+              width: 300,
+              height: 300,
+              fontSize: 32,
+              border: "5px solid lightgray",
+            }}
+          >
+            <h3>От: {device.price} руб.</h3>
+
+            <Button variant="outline-dark">Добавить в корзину</Button>
+          </Card>
+        </Col>
+      </Row>
+
+      <Row className="mx-0 my-3">
+        <h1>Характеристики</h1>
+
+        {description.map((info, index) => {
+          return (
+            <div
+              key={info.id}
+              style={{
+                background: index % 2 === 0 ? "lightgray" : "transparent",
+                padding: 10,
+              }}
+            >
+              {info.title}: {info.description}
+            </div>
+          );
+        })}
+      </Row>
+    </Container>
   );
 };
 
