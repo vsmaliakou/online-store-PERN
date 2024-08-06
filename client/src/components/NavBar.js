127f2c34cdab1cdee1bddab9f30bf2c8bc39e278
vsmaliakou <vsmaliakou@yandex.by>
2024-08-06 11:58:59 +0300

    feat: Registration, authorization page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { observer } from "mobx-react-lite";
-import { Navbar, Nav, Button, Container } from 'react-bootstrap';
-import { Context } from '..';
+import { Navbar, Nav, Button, Container } from "react-bootstrap";
+import { Context } from "..";
 import { NavLink } from "react-router-dom";
 import { SHOP_ROUTE } from "../utils/consts";
 
@@ -11,17 +11,30 @@ const NavBar = observer(() => {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <NavLink style={{ color: 'white' }} to={SHOP_ROUTE}>КупиДевайс</NavLink>
+        <NavLink style={{ color: "white" }} to={SHOP_ROUTE}>
+          КупиДевайс
+        </NavLink>
 
         {user.isAuth ? (
-          <Nav className="ms-auto" style={{ color: 'white' }}>
+          <Nav className="ms-auto" style={{ color: "white" }}>
             <Button variant="outline-light">Админ панель</Button>
 
-            <Button className="ms-2" variant="outline-light" onClick={() => user.setIsAuth(false)}>Выйти</Button>
+            <Button
+              className="ms-2"
+              variant="outline-light"
+              onClick={() => user.setIsAuth(false)}
+            >
+              Выйти
+            </Button>
           </Nav>
         ) : (
-          <Nav className="ms-auto" style={{ color: 'white' }}>
-            <Button variant="outline-light" onClick={() => user.setIsAuth(true)}>Авторизация</Button>
+          <Nav className="ms-auto" style={{ color: "white" }}>
+            <Button
+              variant="outline-light"
+              onClick={() => user.setIsAuth(true)}
+            >
+              Авторизация
+            </Button>
           </Nav>
         )}
       </Container>
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,10 +1,44 @@
 import React from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import { Button, Card, Container, Form, Row } from "react-bootstrap";
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from "../utils/consts";
 
 const Auth = () => {
+  const location = useLocation();
+  const isLogin = location.pathname === LOGIN_ROUTE;
+
   return (
-    <div>
-      AUTH
-    </div>
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: window.innerHeight - 54 }}
+    >
+      <Card style={{ width: 600 }} className="p-5">
+        <h2 className="m-auto">{isLogin ? "Авторизация" : "Регистрация"}</h2>
+
+        <Form className="d-flex flex-column">
+          <Form.Control className="mt-3" placeholder="Введите ваш email..." />
+
+          <Form.Control className="mt-3" placeholder="Введите ваш пароль..." />
+
+          <Row className="mt-3 justify-content-between align-items-center ps-2 pe-2">
+            {isLogin ? (
+              <div className="d-flex w-auto gap-1">
+                Нет аккаунта?{" "}
+                <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся!</NavLink>
+              </div>
+            ) : (
+              <div className="d-flex w-auto gap-1">
+                Есть аккаунт? <NavLink to={LOGIN_ROUTE}>Войдите!</NavLink>
+              </div>
+            )}
+
+            <Button className="w-auto" variant="outline-success">
+              {isLogin ? "Войти" : "Регистрация"}
+            </Button>
+          </Row>
+        </Form>
+      </Card>
+    </Container>
   );
 };
 
